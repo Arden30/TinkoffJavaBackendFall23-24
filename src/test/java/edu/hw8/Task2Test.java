e983f0c0e97e221c5c46d9c27a22eee501681970
Arsenev Denis <denis.arseniev1995@gmail.com>
2023-12-03 20:35:53 +0300

    fixes

--- src/test/java/edu/hw8/Task2Test.java
+++ src/test/java/edu/hw8/Task2Test.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task2Test {
-    private final int n = 30;
+    private final int n = 19;
     private volatile long[] fibs;
     private FixedThreadPool fixedThreadPool;
-    private final long[] fibAll = {0L, 1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L, 55L, 89L, 144L, 233L, 377L, 610L, 987L, 1597L, 2584L, 4181L, 6765L, 10946L, 17711L, 28657L, 46368L, 75025L, 121393L, 196418L, 317811L, 514229L};
+    private final long[] fibAll = {0L, 1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L, 55L, 89L, 144L, 233L, 377L, 610L, 987L, 1597L, 2584L};
     @Test
     @DisplayName("Test 1 (multithreading)")
     void test1() {
