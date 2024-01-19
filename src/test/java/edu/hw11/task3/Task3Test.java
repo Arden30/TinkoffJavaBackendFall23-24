bd2fb940567c159c5a9a39d86a47f0f4903e4caf
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-01-19 15:36:29 +0300

    fix in test

--- src/test/java/edu/hw11/task3/Task3Test.java
+++ src/test/java/edu/hw11/task3/Task3Test.java
@@ -3,20 +3,17 @@ package edu.hw11.task3;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.implementation.Implementation;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import java.lang.reflect.Modifier;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Task3Test {
-    private static final long[] FIBONACCI_NUMBERS = {
-        0L, 1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L
-    };
     private static final String CLASS_NAME = "FibonacciCalculator";
     private static final String METHOD_NAME = "fibonacci";
 
     @ParameterizedTest
-    @ValueSource(ints = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })
-    void createClassWithASM(int n) throws Exception {
+    @CsvSource(value = {"0, 0", "1, 1", "2, 1", "3, 2", "4, 3", "5, 5", "6, 8", "7, 13", "8, 21", "9, 34"})
+    void createClassWithASM(int n, long fib) throws Exception {
         var fibonacciCalculator = new ByteBuddy()
             .subclass(Object.class)
             .name(CLASS_NAME)
@@ -31,6 +28,6 @@ public class Task3Test {
             .getMethod(METHOD_NAME, int.class)
             .invoke(null, n);
 
-        assertThat(result).isEqualTo(FIBONACCI_NUMBERS[n]);
+        assertThat(result).isEqualTo(fib);
     }
 }
